c044311e7a049cb39c9b33a8fcb98c751dea8d19
Arash Rostami <arashrost4mi@gmail.com>
2023-07-21 19:56:32 +0330

    some change

--- task-management-system/src/routes/taskRoutes.js
+++ task-management-system/src/routes/taskRoutes.js
@@ -7,13 +7,14 @@ import {
   deleteTask
 } from '../controllers/taskController.js';
 import authenticateUser from '../middleware/authentication.js';
+import authorize from '../middleware/authorization.js';
 
 const router = express.Router();
 
 router.post('/task', authenticateUser, createTask);
 router.get('/tasks',authenticateUser, getAllTasks);
 router.get('/task/:id',authenticateUser, getTaskById);
-router.put('/task/:id',authenticateUser, updateTask);
-router.delete('/task/:id',authenticateUser, deleteTask);
+router.put('/task/:id',authenticateUser, authorize(['admin']), updateTask);
+router.delete('/task/:id',authenticateUser, authorize(['admin']), deleteTask);
 
 export default router;
